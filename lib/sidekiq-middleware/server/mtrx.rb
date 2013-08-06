16751e9e3267764d2cb13e3421a28b95ad569750
Peter Bui <peter@paydrotalks.com>
2013-08-06 16:33:30 -0700

    Add a prefix for all mtrx STATSD timers
    Usage:
    
      config.server_middleware do |chain|
        chain.add Sidekiq::Middleware::Server::Mtrx, {:prefix => "mtrx"}
      end

--- lib/sidekiq-middleware/server/mtrx.rb
+++ lib/sidekiq-middleware/server/mtrx.rb
@@ -3,26 +3,28 @@ module Sidekiq
     module Server
       class Mtrx
 
+        def initialize(options = {})
+          @prefix = options[:prefix]
+          @prefix << "." if @prefix
+        end
+
         def call(*args)
           Sidekiq::Logging.with_context("#{args[0].class.to_s} MSG-#{args[0].object_id.to_s(36)}") do
-            mtrx_name = worker_instance.class.get_sidekiq_options['mtrx_name']
-            mtrx_name += "." if mtrx_name
-
-            job_name = "#{mtrx_name}#{args[0].class.to_s.underscore}"
+            job_name = args[0].class.to_s.underscore
             queue_name = args[1]['queue']
             delay_ms = elapsed_ms(Time.at(args[1]['queued_at']))
 
             begin
-              STATSD.timer("job.delay.#{job_name}", delay_ms)
-              STATSD.timer("queue.delay.#{queue_name}", delay_ms)
+              STATSD.timer("#{@prefix}job.delay.#{job_name}", delay_ms)
+              STATSD.timer("#{@prefix}queue.delay.#{queue_name}", delay_ms)
               start = Time.now
               yield
-              STATSD.counter("job.#{job_name}.success")
+              STATSD.counter("#{@prefix}job.#{job_name}.success")
             rescue Exception
-              STATSD.counter("job.#{job_name}.error")
+              STATSD.counter("#{@prefix}job.#{job_name}.error")
               raise
             ensure
-              STATSD.timer("job.#{job_name}", elapsed_ms(start))
+              STATSD.timer("#{@prefix}job.#{job_name}", elapsed_ms(start))
             end
           end
         end
