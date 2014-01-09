52c8a3c6990329c7ef800562815ec1c25fd564ce
Remi Gabillet <rgabillet@gmail.com>
2014-01-09 13:46:05 -0800

    should fix Mtrx metric for sidekiq time in queue

--- lib/sidekiq-middleware/client/mtrx.rb
+++ lib/sidekiq-middleware/client/mtrx.rb
@@ -4,6 +4,8 @@ module Sidekiq
       class Mtrx
 
         def call(worker_class, item, queue)
+          # queued_at is the time it entered the queue, if performed in 1 hour, it will be 1 hour from now
+
           # item[:queued_at] = Time.now.utc.to_i
           item[:queued_at] = item['at'] ? item['at'].to_i : Time.now.to_i
           yield 
