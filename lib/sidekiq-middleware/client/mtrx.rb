8196b2f0afda5a7b72f79586e6fd3295307cd36b
Peter Bui <peter@paydrotalks.com>
2014-02-10 12:08:42 -0800

    Only set :queued_at for jobs being performed now
    The scheduled jobs have the :queued_at attribute set when they are
    removed from the scheduled queue and placed into the proper queue. This
    happens in our fork of sidekiq.

--- lib/sidekiq-middleware/client/mtrx.rb
+++ lib/sidekiq-middleware/client/mtrx.rb
@@ -4,11 +4,16 @@ module Sidekiq
       class Mtrx
 
         def call(worker_class, item, queue)
-          # queued_at is the time it entered the queue, if performed in 1 hour, it will be 1 hour from now
+          # See the Sidekiq::Middleware::Server::Mtrx for use of queued_at.
+          #
+          # We only add queued_at when the job is queued right away; for jobs
+          # queued in the future, the queued_at attribute is set when the
+          # Sidekiq::Poller adds the job into the main queues.
+          if !item['at']
+            item[:queued_at] = Time.now.to_i
+          end
 
-          # item[:queued_at] = Time.now.utc.to_i
-          item[:queued_at] = item['at'] ? item['at'].to_i : Time.now.to_i
-          yield 
+          yield
         end
 
       end
